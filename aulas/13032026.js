3bddbcd686d931b40f16b9aa13463f312b931671
anajuliarissa <ana.rissa.silva@escola.pr.gov.br>
2026-03-16 12:10:36 -0300

    crase

--- aulas/13032026.js
+++ aulas/13032026.js
@@ -9,3 +9,14 @@ soma(lucro,lucro2)
 function resultadoDivisaoSocial(lucro,lucro2){
     return soma(lucro,lucro2)/2
 }
+
+function calculadora(ari,a,b){
+
+    switch(ari){
+        case '+':
+            resultado = a + b;
+            break;
+        case '-':
+            
+    }
+}
