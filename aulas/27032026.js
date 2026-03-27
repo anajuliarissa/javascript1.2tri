6e58e3bb7874fe6b091e2856d475321ebd231121
anajuliarissa <ana.rissa.silva@escola.pr.gov.br>
2026-03-27 07:56:29 -0300

    updait

---
+++ aulas/27032026.js
@@ -0,0 +1,15 @@
+const filaNumber = [1,2,3,4,5,6];
+
+const filaStringAmigos = ["Ana Julia", "Maisa", "Yzadra"]
+
+function lerAmigos(filaStringAmigos){
+    console.log('Você tem ${filaStringAmigos.length} amigos')
+
+        for(let i = 0; i < filaStringAmigos.length;i++){
+            console.log(filaStringAmigos[i])
+        }
+}
+
+    
+
+    
